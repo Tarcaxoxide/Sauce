12f95c44a43d5f724e57fac1cf93b39a4ff20c8a
Tarcaxoxide <loganer@vivaldi.net>
2021-08-26 21:13:18 -0400

    added w and l variants of out/in b; added a class for conveniences.

--- hdr/IO.hpp
+++ hdr/IO.hpp
@@ -14,8 +14,54 @@
 
 namespace Sauce{
     namespace IO{
-		void outb(unsigned short port,unsigned char val);
-		unsigned char inb(unsigned short port);
+		void outb(uint16_t port,uint8_t val,bool slow=false);
+		uint8_t inb(uint16_t port);
+		void outw(uint16_t port,uint16_t val);
+		uint16_t inw(uint16_t port);
+		void outl(uint16_t port,uint32_t val);
+		uint32_t inl(uint16_t port);
 		void RemapPic();
+	};
+	namespace EIO{ // E for Extended or Extra , basically it's not neccesary but it's nice to have.
+		class _Port{
+			protected:
+				uint16_t port_number;
+				_Port(uint16_t port_number);
+				~_Port();
+		};
+
+		class _8Bit_Port : public _Port{
+			public:
+				_8Bit_Port(uint16_t port_number);
+				~_8Bit_Port();
+				virtual void operator<<(uint8_t Data);
+				virtual void operator>>(uint8_t &Data);
+
+		};
+
+		class _8BitSlow_Port : public _8Bit_Port{
+			public:
+				_8BitSlow_Port(uint16_t port_number);
+				~_8BitSlow_Port();
+				virtual void operator<<(uint8_t Data);
+
+		};
+
+		class _16Bit_Port : public _Port{
+			public:
+				_16Bit_Port(uint16_t port_number);
+				~_16Bit_Port();
+				virtual void operator<<(uint16_t Data);
+				virtual void operator>>(uint16_t &Data);
+				
+		};
+
+		class _32Bit_Port : public _Port{
+			public:
+				_32Bit_Port(uint16_t port_number);
+				~_32Bit_Port();
+				virtual void operator<<(uint32_t Data);
+				virtual void operator>>(uint32_t &Data);
+		};
 	};
 };
--- src/IO.cpp
+++ src/IO.cpp
@@ -4,12 +4,36 @@
 
 namespace Sauce{
     namespace IO{
-        void outb(unsigned short port,unsigned char val){
-            asm volatile ("outb %0, %1": : "a"(val), "Nd"(port));
+        // 8bit
+        void outb(uint16_t port,uint8_t val,bool slow){
+            if(slow){
+                asm volatile ("outb %0, %1\njmp 1f\n1: jmp 1f\n1:": : "a"(val), "Nd"(port));
+            }else{
+                asm volatile ("outb %0, %1": : "a"(val), "Nd"(port));
+            }
         }
-        unsigned char inb(unsigned short port){
-            unsigned char Result=NULL;
+        uint8_t inb(uint16_t port){
+            uint8_t Result=NULL;
             asm volatile("inb %1, %0": "=a"(Result): "Nd"(port));
+            return Result;
+        }
+        // 16bit
+        void outw(uint16_t port,uint16_t val){
+            asm volatile ("outw %0, %1": : "a"(val), "Nd"(port));
+        }
+        uint16_t inw(uint16_t port){
+            uint16_t Result=NULL;
+            asm volatile("inw %1, %0": "=a"(Result): "Nd"(port));
+            return Result;
+        }
+        // 32bit
+        void outl(uint16_t port,uint32_t val){
+                asm volatile ("outl %0, %1": : "a"(val), "Nd"(port));
+
+        }
+        uint32_t inl(uint16_t port){
+            uint32_t Result=NULL;
+            asm volatile("inl %1, %0": "=a"(Result): "Nd"(port));
             return Result;
         }
         void RemapPic(){
@@ -27,6 +51,45 @@ namespace Sauce{
 
             outb(PIC1_DATA,a1);
             outb(PIC2_DATA,a2);
+        }
+    };
+    namespace EIO{
+        _Port::_Port(uint16_t port_number){
+            this->port_number=port_number;
+        }
+        _Port::~_Port(){}
+
+        _8Bit_Port::_8Bit_Port(uint16_t port_number):_Port(port_number){}
+        _8Bit_Port::~_8Bit_Port(){}
+        void _8Bit_Port::operator<<(uint8_t Data){
+            Sauce::IO::outb(port_number,Data);
+        }
+		void _8Bit_Port::operator>>(uint8_t &Data){
+            Data = Sauce::IO::inb(port_number);
+        }
+
+        _8BitSlow_Port::_8BitSlow_Port(uint16_t port_number):_8Bit_Port(port_number){}
+        _8BitSlow_Port::~_8BitSlow_Port(){}
+        void _8BitSlow_Port::operator<<(uint8_t Data){
+            Sauce::IO::outb(port_number,Data,true);
+        }
+
+        _16Bit_Port::_16Bit_Port(uint16_t port_number):_Port(port_number){}
+        _16Bit_Port::~_16Bit_Port(){}
+        void _16Bit_Port::operator<<(uint16_t Data){
+            Sauce::IO::outw(port_number,Data);
+        }
+		void _16Bit_Port::operator>>(uint16_t &Data){
+            Data = Sauce::IO::inw(port_number);
+        }
+
+        _32Bit_Port::_32Bit_Port(uint16_t port_number):_Port(port_number){}
+        _32Bit_Port::~_32Bit_Port(){}
+        void _32Bit_Port::operator<<(uint32_t Data){
+            Sauce::IO::outl(port_number,Data);
+        }
+		void _32Bit_Port::operator>>(uint32_t &Data){
+            Data = Sauce::IO::inl(port_number);
         }
     };
 };
