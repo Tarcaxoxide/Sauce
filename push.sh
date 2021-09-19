8fbfcf57fdc4b4a074d5d56c10d8a861d854a3c8
Tarcaxoxide <loganer@vivaldi.net>
2021-09-19 01:31:20 -0400

    modified:   push.sh

--- push.sh
+++ push.sh
@@ -3,4 +3,4 @@ git add .
 export EDITOR='vim' 
 git commit -a
 read -p "press enter to push."
-git push "https://loganer%40vivaldi.net:$(cat ../git/token.txt)@github.com/Tarcaxoxide/Sauce.git"
+git push "https://loganer%40vivaldi.net:$(cat ../git_token.txt)@github.com/Tarcaxoxide/Sauce.git"
