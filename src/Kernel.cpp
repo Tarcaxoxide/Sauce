db155e4f8cde5bca30fde09009d4a3b887f7eae0
Tarcaxoxide <loganer@vivaldi.net>
2021-09-29 23:54:31 -0400

    modified:   src/Kernel.cpp

--- src/Kernel.cpp
+++ src/Kernel.cpp
@@ -10,7 +10,6 @@ void Notify_Kernel_Of_KeyPress(Sauce::Keyboard::KeyboardKey _Key){
 }
 
 void Notify_Kernel_Of_Timer(float rate){
-    //Sauce::Interrupts::ProgrammableInterruptTimerCounter+=0x0000000000000001;
     Sauce::Interrupts::ProgrammableInterruptTimerCounter+=rate;
         
     if(Sauce::Interrupts::ProgrammableInterruptTimerCounter > 0x1000000000000000){
@@ -18,12 +17,8 @@ void Notify_Kernel_Of_Timer(float rate){
     }
 }
 
-
-
 void Kernel_Main(){
     Sauce::Terminal::String(Sauce::Convert::To_String::From_uint64(Sauce::Memory::GetFreeHeap()));
     Sauce::Terminal::String(" \n\r");
-
-
     Sauce::STOP(false);
 }
