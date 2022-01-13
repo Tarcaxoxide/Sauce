3fef8f65f95cea0cd66f2ec36b2963e8c0bb00ba
Tarcaxoxide <loganer@vivaldi.net>
2022-01-13 14:51:07 -0500

    modified:   hdr/Misc.hpp 	modified:   src/Kernel.cpp 	modified:   src/Misc.cpp

--- src/Kernel.cpp
+++ src/Kernel.cpp
@@ -24,5 +24,6 @@ void Notify_Kernel_Of_Timer(float rate){
 
 void Kernel_Main(){
     Memory::PrintMemoryMap_All();
+
     STOP(Nothing);
 }
--- src/Misc.cpp
+++ src/Misc.cpp
@@ -15,14 +15,23 @@ namespace Sauce{
     void STOP(ERROR_CODES code){
         Sauce::Terminal::String("\n\r [");
         Sauce::Terminal::String(Sauce::Convert::To_String::From_uint64(code));
-        Sauce::Terminal::String("] System halted");
+	Sauce::Terminal::String("] System halted");
+	if(code != Nothing){
+		Sauce::Terminal::String(" and stopped interrupts ");
+	}
+	if(code == Nothing)Sauce::Terminal::String("(Ok)");
+	if(code == Index_too_big)Sauce::Terminal::String("(Index too big)");
+	if(code == Shape_mismatch)Sauce::Terminal::String("(Shape mismatch)");
+	if(code == Out_of_memory)Sauce::Terminal::String("(Out of memory)");
+	if(code == Combined_segment_with_null)Sauce::Terminal::String("(Combined segment with null)");
+	if(code == Alignment_unspecified)Sauce::Terminal::String("(Alignment unspecified)");
+	if(code == Size_unspecified)Sauce::Terminal::String("(Size unspecified)");
+	if(code == User_Executed)Sauce::Terminal::String("(User Executed)");
+
         while(true){
-            if(code){
-                asm volatile("cli");// stop interrupts.
-                Sauce::Terminal::String(" and stopped interrupts");
-            }
-            Sauce::Terminal::String(" \n\r");
-            asm volatile("hlt"); // halt the cpu.
+            Sauce::Terminal::String(".\n\r");
+	    if(code != Nothing)asm volatile("cli");
+	    asm volatile("hlt"); // halt the cpu.
         }
     }
     void REBOOT(){
