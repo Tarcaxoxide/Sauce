849212ef811625dd608206daf2425c069d9d356f
Tarcaxoxide <loganer@vivaldi.net>
2022-05-20 22:32:08 -0400

    Fri May 20 10:32:08 PM EDT 2022

--- inc/Sauce/Graphics/Font.hpp
+++ inc/Sauce/Graphics/Font.hpp
@@ -5,7 +5,9 @@
 namespace Sauce{
     namespace Graphics{
         namespace SauceFont{
-            const static size_t GlyphSize=7*7;
+            const static size_t GlyphWidth=13;
+            const static size_t GlyphHeight=13;
+            const static size_t GlyphSize=GlyphWidth*GlyphHeight;
             extern uint8_t Glyphs[][GlyphSize];
         };
     };
--- kernel/src/Graphics/Shell.cpp
+++ kernel/src/Graphics/Shell.cpp
@@ -9,21 +9,21 @@ namespace Sauce{
         }
 
         void Shell_cl::PutChar(unsigned char chr){
-            for(size_t X=0;X<7;X++){
-                for(size_t Y=0;Y<7;Y++){
+            for(size_t X=0;X<Sauce::Graphics::SauceFont::GlyphWidth;X++){
+                for(size_t Y=0;Y<Sauce::Graphics::SauceFont::GlyphHeight;Y++){
                     GOP_PixelStructure ThisColor{0,0,0,0xFF};
                     
                     ThisColor.Red=ForegroundColor.Red/9;
                     ThisColor.Green=ForegroundColor.Green/9;
                     ThisColor.Blue=ForegroundColor.Blue/9;
-                    ThisColor.Red*=Sauce::Graphics::SauceFont::Glyphs[chr][Sauce::ind(X+Cursor.X,Y+Cursor.Y,7)];
-                    ThisColor.Green*=Sauce::Graphics::SauceFont::Glyphs[chr][Sauce::ind(X+Cursor.X,Y+Cursor.Y,7)];
-                    ThisColor.Blue*=Sauce::Graphics::SauceFont::Glyphs[chr][Sauce::ind(X+Cursor.X,Y+Cursor.Y,7)];
+                    ThisColor.Red*=Sauce::Graphics::SauceFont::Glyphs[chr][Sauce::ind(X+Cursor.X,Y+Cursor.Y,Sauce::Graphics::SauceFont::GlyphWidth)];
+                    ThisColor.Green*=Sauce::Graphics::SauceFont::Glyphs[chr][Sauce::ind(X+Cursor.X,Y+Cursor.Y,Sauce::Graphics::SauceFont::GlyphWidth)];
+                    ThisColor.Blue*=Sauce::Graphics::SauceFont::Glyphs[chr][Sauce::ind(X+Cursor.X,Y+Cursor.Y,Sauce::Graphics::SauceFont::GlyphWidth)];
 
                     PixelBuffer[Sauce::ind(X+Cursor.X,Y+Cursor.Y,PixelsPerLine)]=ThisColor;
                 }
             }
-            Cursor.X+=7;
+            Cursor.X+=Sauce::Graphics::SauceFont::GlyphWidth;
         }
         
     };
