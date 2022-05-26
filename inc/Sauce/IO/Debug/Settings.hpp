4c157c9cb02174575328d35ec33219a6ffd1a066
Tarcaxoxide <loganer@vivaldi.net>
2022-05-26 18:41:50 -0400

    Thu May 26 06:41:50 PM EDT 2022

--- inc/Sauce/IO/Debug/Settings.hpp
+++ inc/Sauce/IO/Debug/Settings.hpp
@@ -1,7 +1,7 @@
 namespace Sauce{
     namespace IO{
         namespace Debug{ // General overall settings
-            static const bool FUNCTION_CALLS=false; // the first print of every function, this just tells us what function we are currently executing.
+            static const bool FUNCTION_CALLS=true; // the first print of every function, this just tells us what function we are currently executing.
             static const bool FUNCTION_DETAILS=false; // anything after the FUNCTION_CALLS and before the FUNCTION_RETURNS.
             static const bool FUNCTION_RETURNS=false; // the value or values if a structure, we are returning to the calling function.
         };
@@ -14,8 +14,8 @@ namespace Sauce{
             static const bool INTERRUPT_PIT=false;
             static const bool INTERRUPT_FAULTS=false;
             static const bool BITMAP=false;
-            static const bool MOUSE=false;
-            static const bool KEYBOARD=false;
+            static const bool MOUSE=true;
+            static const bool KEYBOARD=true;
             static const bool SHELL=false;
             static const bool TERMINAL=false;
             static const bool INTERRUPT_DESCRIPTOR_TABLE=false;
