75c4d4c66893cda051ea32387e1f80caf645db4d
Tarcaxoxide <loganer@vivaldi.net>
2022-05-29 13:20:12 -0400

    Sun May 29 01:20:12 PM EDT 2022

--- inc/Sauce/Storage/AHCI.hpp
+++ inc/Sauce/Storage/AHCI.hpp
@@ -7,6 +7,29 @@
 
 namespace Sauce{
     namespace Storage{
+        #define HBA_PORT_DEVICE_PRESENT 0x3
+        #define HBA_PORT_IPM_ACTIVE 0x1
+        #define SATA_SIG_ATAPI 0xEB140101
+        #define SATA_SIG_ATA 0x00000101
+        #define SATA_SIG_SEMB 0xC33C0101
+        #define SATA_SIG_PM 0x96690101
+        #define HBA_PxCMD_CR 0x8000
+        #define HBA_PxCMD_FRE 0x0010
+        #define HBA_PxCMD_FR 0x4000
+        #define HBA_PxCMD_ST 0x0001
+        #define HBA_PxIS_TFES (1 << 30)
+        #define FIS_TYPE_REG_H2D 0x27
+        #define FIS_TYPE_REG_D2H 0x34
+        #define FIS_TYPE_DMA_ACT 0x39
+        #define FIS_TYPE_DMA_ 0x41
+        #define FIS_TYPE_DATA 0x46
+        #define FIS_TYPE_BIST 0x58
+        #define FIS_TYPE_PIO_SETUP 0x5F
+        #define FIS_TYPE_DEV_BITS 0xA1
+        #define ATA_CMD_READ_DMA_EX 0x25
+        #define ATA_DEV_BUSY 0x80
+        #define ATA_DEV_DRQ 0x08
+
         enum HBAPortType{
             NONE=0,
             SATA,
@@ -72,6 +95,44 @@ namespace Sauce{
             HBAPort_st ports[1];
         };
 
+        struct HBAPRDTEntry_st{
+            uint32_t dataBaseAddress;
+            uint32_t dataBaseAddressUpper;
+            uint32_t rsv0;
+            uint32_t byteCount:22;
+            uint32_t rsv1:9;
+            uint32_t interruptOnCompletion:1;
+        };
+
+        struct HBACommandTable_st{
+            uint8_t commandFIS[64];
+            uint8_t atapiCommand[16];
+            uint8_t rsv[48];
+            HBAPRDTEntry_st prdtEntry[];
+        };
+
+        struct FIS_REG_H2D_st{
+            uint8_t fisType;
+            uint8_t portMultiplier:4;
+            uint8_t rsv0:3;
+            uint8_t commandControl:1;
+            uint8_t command;
+            uint8_t featureLow;
+            uint8_t lba0;
+            uint8_t lba1;
+            uint8_t lba2;
+            uint8_t deviceRegister;
+            uint8_t lba3;
+            uint8_t lba4;
+            uint8_t lba5;
+            uint8_t featureHigh;
+            uint8_t countLow;
+            uint8_t countHigh;
+            uint8_t isoCommandCompletion;
+            uint8_t control;
+            uint8_t rsv1[4];
+        };
+
         struct ParsedHBAPort_st{
             HBAPort_st* Address=nullptr;
             uint8_t* buffer=nullptr;
@@ -82,6 +143,7 @@ namespace Sauce{
             void Configure();
             void StartCMD();
             void StopCMD();
+            bool Read(uint64_t sector,uint16_t sectorCount,void* buffer);
         };
 
         ParsedHBAPort_st CheckPortType(HBAPort_st* port);
--- kernel/src/Storage/AHCI.cpp
+++ kernel/src/Storage/AHCI.cpp
@@ -5,20 +5,6 @@
 
 namespace Sauce{
     namespace Storage{
-
-        #define HBA_PORT_DEVICE_PRESENT 0x3
-        #define HBA_PORT_IPM_ACTIVE 0x1
-        #define SATA_SIG_ATAPI 0xEB140101
-        #define SATA_SIG_ATA 0x00000101
-        #define SATA_SIG_SEMB 0xC33C0101
-        #define SATA_SIG_PM 0x96690101
-
-        #define HBA_PxCMD_CR 0x8000
-        #define HBA_PxCMD_FRE 0x0010
-        #define HBA_PxCMD_FR 0x4000
-        #define HBA_PxCMD_ST 0x0001
-
-
         ParsedHBAPort_st CheckPortType(HBAPort_st* port){
             Sauce::IO::Debug::Print_Call("CheckPortType",Sauce::IO::Debug::STORAGE);
             ParsedHBAPort_st ParsedHBAPort;
@@ -61,7 +47,6 @@ namespace Sauce{
             }
             return ParsedHBAPort;
         }
-
         void ParsedHBAPort_st::Configure(){
             Sauce::IO::Debug::Print_Call("ParsedHBAPort_st::Configure",Sauce::IO::Debug::STORAGE);
             StopCMD();
@@ -104,6 +89,54 @@ namespace Sauce{
             while((Address->cmdSts & HBA_PxCMD_FR || Address->cmdSts & HBA_PxCMD_CR));
             Sauce::IO::Debug::Print_Return("void",Sauce::IO::Debug::STORAGE);
         }
+        bool ParsedHBAPort_st::Read(uint64_t sector,uint16_t sectorCount,void* buffer){
+            Sauce::IO::Debug::Print_Call("ParsedHBAPort_st::Read",Sauce::IO::Debug::STORAGE);
+
+            size_t spin=0;
+            while((Address->taskFileData & (ATA_DEV_BUSY | ATA_DEV_DRQ)) && spin < 1000000){spin++;}
+
+            if(spin == 1000000){Sauce::IO::Debug::Print_Return("False",Sauce::IO::Debug::STORAGE);return false;}
+
+            uint32_t sectorL = (uint32_t)sector;
+            uint32_t sectorH = (uint32_t)(sector >> 32);
+            Address->interruptStatus = (uint32_t)-1; // Clear pending interrupt bits
+
+            HBACommandHeader_st* cmdHeader = (HBACommandHeader_st*)(uint64_t)Address->commandListBase;
+            cmdHeader->commandFISLength = sizeof(FIS_REG_H2D_st)/sizeof(uint32_t); // command FIS size
+            cmdHeader->write=0; // this is a read.
+            cmdHeader->prdtLength=1;
+
+            HBACommandTable_st* commandTable = (HBACommandTable_st*)((uint64_t)cmdHeader->commandTableBaseAddress);
+            Sauce::Memory::memset(commandTable,0,sizeof(HBACommandTable_st)+(cmdHeader->prdtLength-1)*sizeof(HBAPRDTEntry_st));
+
+            commandTable->prdtEntry[0].dataBaseAddress = (uint32_t)(uint64_t)buffer;
+            commandTable->prdtEntry[0].dataBaseAddressUpper = (uint32_t)((uint64_t)buffer >> 32);
+            commandTable->prdtEntry[0].byteCount = (sectorCount<<9)-1; // 512 per sector
+            commandTable->prdtEntry[0].interruptOnCompletion=1;
+
+            FIS_REG_H2D_st* cmdFIS = (FIS_REG_H2D_st*)(&commandTable->commandFIS);
+            cmdFIS->fisType = FIS_TYPE_REG_H2D;
+            cmdFIS->commandControl = 1; // command
+            cmdFIS->command = ATA_CMD_READ_DMA_EX;
+
+            cmdFIS->lba0 = (uint8_t)sectorL;
+            cmdFIS->lba1 = (uint8_t)(sectorL >> 8);
+            cmdFIS->lba2 = (uint8_t)(sectorL >> 16);
+            cmdFIS->lba3 = (uint8_t)sectorH;
+            cmdFIS->lba4 = (uint8_t)(sectorH >> 8);
+            cmdFIS->lba5 = (uint8_t)(sectorH >> 16);
+            cmdFIS->deviceRegister = 1<<6; // LBA mode
+
+            cmdFIS->countLow = sectorCount & 0xFF;
+            cmdFIS->countHigh = (sectorCount >> 8) & 0xFF;
+
+            Address->commandIssue = 1;
+
+            while(!(Address->commandIssue == 0)){if(Address->interruptStatus & HBA_PxIS_TFES){Sauce::IO::Debug::Print_Return("False",Sauce::IO::Debug::STORAGE);return false;}}
+
+            Sauce::IO::Debug::Print_Return("True",Sauce::IO::Debug::STORAGE);
+            return true;
+        }
 
         AHCIDriver_cl::AHCIDriver_cl(Sauce::IO::PCIDeviceHeader_st* pciBaseAddress){
             Sauce::IO::Debug::Print_Call("AHCIDriver_cl::AHCIDriver_cl",Sauce::IO::Debug::STORAGE);
@@ -111,6 +144,27 @@ namespace Sauce{
             ABAR = (HBAMemory_st*)((uint64_t)((Sauce::IO::PCIHeader0_st*)pciBaseAddress)->BAR5);
             Sauce::Global::PageTableManager.MapMemory(ABAR,ABAR);
             ProbePorts();
+
+            /*Testing*/{
+                Sauce::IO::Debug::Print_Detail("#TEST START#",Sauce::IO::Debug::STORAGE);
+                for(int i=0;i<HBAPorts.Size();i++){
+                    {
+                        if(HBAPorts[i].Type != HBAPortType::NONE){
+                            HBAPorts[i].buffer = (uint8_t*)Sauce::Global::PageFrameAllocator.RequestPage();
+                            Sauce::Memory::memset(HBAPorts[i].buffer,0,0x1000);
+                            HBAPorts[i].Read(0,4,HBAPorts[i].buffer);
+                            char BuffChars[1025]{0};
+                            for(int t=0;t<1024;t++){
+                                BuffChars[t]=HBAPorts[i].buffer[t];
+                            }
+                            Sauce::IO::Debug::Print_Detail(BuffChars,Sauce::IO::Debug::STORAGE);
+                        }
+                        
+                    }
+                }
+                Sauce::IO::Debug::Print_Detail("#TEST END#",Sauce::IO::Debug::STORAGE);
+            }
+
             Sauce::IO::Debug::Print_Return("this",Sauce::IO::Debug::STORAGE);
         }
         void AHCIDriver_cl::ProbePorts(){
@@ -123,7 +177,7 @@ namespace Sauce{
                     }
                 }
             }
-            for(int i=0;i<32;i++){
+            for(int i=0;i<HBAPorts.Size();i++){
                 switch(HBAPorts[i].Type){
                     case HBAPortType::NONE:{
                         Sauce::IO::Debug::Print_Detail("HBAPort#",Sauce::IO::Debug::STORAGE,Sauce::IO::Debug::StartOfPrint::Start);
@@ -134,21 +188,25 @@ namespace Sauce{
                         Sauce::IO::Debug::Print_Detail("HBAPort#",Sauce::IO::Debug::STORAGE,Sauce::IO::Debug::StartOfPrint::Start);
                         Sauce::IO::Debug::Print_Detail(Sauce::Convert::ToString(i),Sauce::IO::Debug::STORAGE,Sauce::IO::Debug::StartOfPrint::Middle);
                         Sauce::IO::Debug::Print_Detail(",HBAPortType::SATA",Sauce::IO::Debug::STORAGE,Sauce::IO::Debug::StartOfPrint::End);
+                        HBAPorts[i].Configure();
                     }break;
                     case HBAPortType::SEMB:{
                         Sauce::IO::Debug::Print_Detail("HBAPort#",Sauce::IO::Debug::STORAGE,Sauce::IO::Debug::StartOfPrint::Start);
                         Sauce::IO::Debug::Print_Detail(Sauce::Convert::ToString(i),Sauce::IO::Debug::STORAGE,Sauce::IO::Debug::StartOfPrint::Middle);
                         Sauce::IO::Debug::Print_Detail(",HBAPortType::SEMB",Sauce::IO::Debug::STORAGE,Sauce::IO::Debug::StartOfPrint::End);
+                        HBAPorts[i].Configure();
                     }break;
                     case HBAPortType::PM:{
                         Sauce::IO::Debug::Print_Detail("HBAPort#",Sauce::IO::Debug::STORAGE,Sauce::IO::Debug::StartOfPrint::Start);
                         Sauce::IO::Debug::Print_Detail(Sauce::Convert::ToString(i),Sauce::IO::Debug::STORAGE,Sauce::IO::Debug::StartOfPrint::Middle);
                         Sauce::IO::Debug::Print_Detail(",HBAPortType::PM",Sauce::IO::Debug::STORAGE,Sauce::IO::Debug::StartOfPrint::End);
+                        HBAPorts[i].Configure();
                     }break;
                     case HBAPortType::SATAPI:{
                         Sauce::IO::Debug::Print_Detail("HBAPort#",Sauce::IO::Debug::STORAGE,Sauce::IO::Debug::StartOfPrint::Start);
                         Sauce::IO::Debug::Print_Detail(Sauce::Convert::ToString(i),Sauce::IO::Debug::STORAGE,Sauce::IO::Debug::StartOfPrint::Middle);
                         Sauce::IO::Debug::Print_Detail(",HBAPortType::SATAPI",Sauce::IO::Debug::STORAGE,Sauce::IO::Debug::StartOfPrint::End);
+                        HBAPorts[i].Configure();
                     }break;
                 }
             }
