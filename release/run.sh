6f869f05a1081edd39833378e0d2188ca5bd3d45
Tarcaxoxide <loganer@vivaldi.net>
2022-09-24 21:24:43 -0400

    Sat Sep 24 09:24:43 PM EDT 2022

--- release/run.sh
+++
@@ -1,3 +0,0 @@
-
-
-qemu-system-x86_64 -display gtk,zoom-to-fit=on -smp 2 -serial stdio -machine q35 -s -drive format=raw,file=Sauce.img -m 2G -cpu qemu64 -drive if=pflash,format=raw,unit=0,file="OVMFbin/OVMF_CODE-pure-efi.fd",readonly=on -drive if=pflash,format=raw,unit=1,file="OVMFbin/OVMF_VARS-pure-efi.fd"
