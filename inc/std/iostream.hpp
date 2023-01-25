871c80d96e929cb3be7d98ab80feb0496215eddd
Tarcaxoxide <loganer@vivaldi.net>
2023-01-25 09:09:36 -0500

    Wed Jan 25 09:09:35 AM EST 2023

--- inc/std/iostream.hpp
+++ inc/std/iostream.hpp
@@ -2,9 +2,10 @@
 #include<Sauce/Memory/DynamicArray.hpp>
 #include<std/string.hpp>
 
-namespace std _GLIBCXX_VISIBILITY(default){
+namespace std /*_GLIBCXX_VISIBILITY(default)*/{
+    //I don't know what this does yet but i'm going to put it here for later.
     struct cout_st{
-        std::string operator<<(std::string str);
+        cout_st& operator<<(std::string nstr);
     };
     extern const char* endl;
     extern cout_st cout;
--- kernel/src/std/iostream.cpp
+++ kernel/src/std/iostream.cpp
@@ -2,9 +2,9 @@
 #include <Sauce/Global.hpp>
 
 namespace std _GLIBCXX_VISIBILITY(default){
-    std::string cout_st::operator<<(std::string str){
+    cout_st& cout_st::operator<<(std::string str){
         Sauce::Global::Shell->PutString(str,false);
-        return str;
+        return *this;
     }
     const char* endl("\n\r");
     cout_st cout;
