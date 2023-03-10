4859f4628e856bb774ec29a5ea397e7b6412b318
Tarcaxoxide <loganer@vivaldi.net>
2023-03-10 18:02:00 -0500

    Fri Mar 10 06:01:59 PM EST 2023

--- inc/Sauce/Filesystem/Blob.hpp
+++ inc/Sauce/Filesystem/Blob.hpp
@@ -11,25 +11,6 @@ namespace Sauce{
 					Header.Name=name;
 					Header.Classification=classification;
 				}
-				std::list<Blob_st> Sub;
-				inline void Add(const char* name,Sauce::Filesystem::Header::Classification classification){
-					Sub.AddLast(Blob_st(name,classification));
-				}
-				inline std::string List(){
-					std::string Result="[";
-					for(size_t i=0;i<Sub.Size();i++){
-						switch(Sub[i].Header.Classification){
-							case Sauce::Filesystem::Header::Classification::Classification_Unset:{Result+="Unset:";}break;
-							case Sauce::Filesystem::Header::Classification::Classification_File:{Result+="File:";}break;
-							case Sauce::Filesystem::Header::Classification::Classification_Directory:{Result+="Directory:";}break;
-							case Sauce::Filesystem::Header::Classification::Classification_Descriptor:{Result+="Descriptor:";}break;
-						}
-						Result+=Sub[i].Header.Name;
-						if(i<Sub.Size()-1)Result+=",";
-					}
-					Result+="]";
-					return Result;
-				}
 			};
 		};
 	};
--- inc/Sauce/Filesystem/Directory.hpp
+++ inc/Sauce/Filesystem/Directory.hpp
@@ -7,8 +7,27 @@ namespace Sauce{
 		namespace Directory{
 			struct Directory_st:public Sauce::Filesystem::Blob::Blob_st{
 				Directory_st(const char* name): Sauce::Filesystem::Blob::Blob_st(name,Sauce::Filesystem::Header::Classification::Classification_Directory){}
+				std::list<Blob_st> Sub;
+				inline void Add(const char* name,Sauce::Filesystem::Header::Classification classification){
+					Sub.AddLast(Blob_st(name,classification));
+				}
 				inline void AddFile(const char* name){Add(name,Sauce::Filesystem::Header::Classification::Classification_File);}
 				inline void AddDirectory(const char* name){Add(name,Sauce::Filesystem::Header::Classification::Classification_Directory);}
+				inline std::string List(){
+					std::string Result="[";
+					for(size_t i=0;i<Sub.Size();i++){
+						switch(Sub[i].Header.Classification){
+							case Sauce::Filesystem::Header::Classification::Classification_Unset:{Result+="Unset:";}break;
+							case Sauce::Filesystem::Header::Classification::Classification_File:{Result+="File:";}break;
+							case Sauce::Filesystem::Header::Classification::Classification_Directory:{Result+="Directory:";}break;
+							case Sauce::Filesystem::Header::Classification::Classification_Descriptor:{Result+="Descriptor:";}break;
+						}
+						Result+=Sub[i].Header.Name;
+						if(i<Sub.Size()-1)Result+=",";
+					}
+					Result+="]";
+					return Result;
+				}
 			};
 		};
 	};
