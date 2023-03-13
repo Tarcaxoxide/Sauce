1e9e134847e12579d41bcd4acd297f3c7ec30632
Tarcaxoxide <loganer@vivaldi.net>
2023-03-13 17:10:42 -0400

    Mon Mar 13 05:10:42 PM EDT 2023

--- inc/Sauce/Filesystem/File.hpp
+++ inc/Sauce/Filesystem/File.hpp
@@ -10,8 +10,7 @@ namespace Sauce{
 				size_t CurrentSector=0;
 				std::ustring CurrentSectorData;
 				File_st(const char* name,const char* ext): Sauce::Filesystem::Blob::Blob_st(name,ext,Sauce::Filesystem::Header::Classification_en::Classification_File){}
-			
-				uint8_t operator[](size_t byte);
+				uint8_t ReadByte(size_t byte);
 			};
 		};
 	};
--- kernel/src/Filesystem/File.cpp
+++ kernel/src/Filesystem/File.cpp
@@ -4,12 +4,15 @@
 namespace Sauce{
 	namespace Filesystem{
 		namespace File{
-			uint8_t File_st::operator[](size_t byte){
-				CurrentByte=byte%Header.BytesPerSector;
-				CurrentSector=Header.Sectors[(byte/Header.BytesPerSector)%Header.Sectors.Size()];
-				CurrentSectorData.Clear();
-				Sauce::Global::Storage::AHCIDrivers.First()->Read(Header.PortNumber,CurrentSector,1/*read 1 sector*/,CurrentSectorData);
-				return CurrentSectorData[CurrentByte];
+			uint8_t File_st::ReadByte(size_t byte){
+				CurrentByte=byte%Header.BytesPerSector;// we get the offset the byte is at within the sector
+				size_t sector=Header.Sectors[(byte/Header.BytesPerSector)%Header.Sectors.Size()];// we get the sector number the byte is in (note:loops back around)
+				if(sector != CurrentSector){// if we are reading from a different sector
+					CurrentSector=sector;
+					CurrentSectorData.Clear(); // we clear the old data
+					Sauce::Global::Storage::AHCIDrivers.First()->Read(Header.PortNumber,CurrentSector,1/*read 1 sector*/,CurrentSectorData); //and read the new sector in
+				}
+				return CurrentSectorData[CurrentByte]; // we return the read byte.
 			}
 		};
 	};
