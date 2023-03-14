b83ce5584d32b96c1b230ea00962cc4ab550072e
Tarcaxoxide <loganer@vivaldi.net>
2023-03-14 15:18:12 -0400

    Tue Mar 14 03:18:11 PM EDT 2023

--- inc/Sauce/Filesystem/Blob.hpp
+++ inc/Sauce/Filesystem/Blob.hpp
@@ -27,20 +27,20 @@ namespace Sauce{
 					Header.Classification=classification;
 				}
 				void Add(const char* name,const char* ext,Sauce::Filesystem::Header::Classification_en classification);
-				Blob_st(const Blob_st& other){
-					Header.Sectors=other.Header.Sectors;
-					Header.BytesPerSector=other.Header.BytesPerSector;
-					Header.PortNumber=other.Header.PortNumber;
-					Header.Name=new char[sizeof(other.Header.Name)]{0};
-					for(size_t i=0;i<sizeof(other.Header.Name);i++)Header.Name[i]=other.Header.Name[i];
-					Header.Ext=new char[sizeof(other.Header.Ext)]{0};
-					for(size_t i=0;i<sizeof(other.Header.Ext);i++)Header.Ext[i]=other.Header.Ext[i];
-					for(size_t i=0;i<other.Sub.Size();i++)Sub.AddLast(Blob_st(other.Sub[i]));
-				}
-				~Blob_st(){
-					delete Header.Name;
-					delete Header.Ext;
-				}
+				//Blob_st(const Blob_st& other){
+				//	Header.Sectors=other.Header.Sectors;
+				//	Header.BytesPerSector=other.Header.BytesPerSector;
+				//	Header.PortNumber=other.Header.PortNumber;
+				//	Header.Name=new char[sizeof(other.Header.Name)]{0};
+				//	for(size_t i=0;i<sizeof(other.Header.Name);i++)Header.Name[i]=other.Header.Name[i];
+				//	Header.Ext=new char[sizeof(other.Header.Ext)]{0};
+				//	for(size_t i=0;i<sizeof(other.Header.Ext);i++)Header.Ext[i]=other.Header.Ext[i];
+				//	for(size_t i=0;i<other.Sub.Size();i++)Sub.AddLast(Blob_st(other.Sub[i]));
+				//}
+				//~Blob_st(){
+				//	delete Header.Name;
+				//	delete Header.Ext;
+				//}
 				std::string List();
 				Blob_st& GetSub(size_t index);
 				uint8_t& GetByte(size_t byte);
