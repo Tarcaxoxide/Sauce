2bcc5dfa642b0f11192dc73b1b9b481ef628740f
Tarcaxoxide <loganer@vivaldi.net>
2023-04-07 09:45:05 -0400

    Fri Apr  7 09:45:05 AM EDT 2023

--- kernel/src/Graphics/Shell/Langauge/Lexer.cpp
+++ kernel/src/Graphics/Shell/Langauge/Lexer.cpp
@@ -26,7 +26,24 @@ namespace Sauce{
 								size_t length=m_index-start;m_index++;
 								return m_input.Substr(start,length+1);
 							}break;
-							case '+':case '-':case '*':case '/':case '=':case '(':case ')':case '{':case '}':case '<':case '>':case '[':case ']':{
+							case '0':case '1':case '2':case '3':case '4':case '5':case '6':case '7':case '8':case '9':{
+								size_t start=m_index;
+								while(++m_index<m_input.Size()&&(m_input[m_index]=='.'
+																||m_input[m_index]=='0'
+																||m_input[m_index]=='1'
+																||m_input[m_index]=='2'
+																||m_input[m_index]=='3'
+																||m_input[m_index]=='4'
+																||m_input[m_index]=='5'
+																||m_input[m_index]=='6'
+																||m_input[m_index]=='7'
+																||m_input[m_index]=='8'
+																||m_input[m_index]=='9'
+																)){}
+								size_t length=m_index-start;
+								return m_input.Substr(start,length);
+							}break;
+							case '.':case '~':case '+':case '-':case '*':case '/':case '=':case '(':case ')':case '{':case '}':case '<':case '>':case '[':case ']':{
 								return m_input.Substr(m_index++,1);
 							}break;
 							default:{
@@ -46,6 +63,7 @@ namespace Sauce{
 																||m_input[m_index]=='>'
 																||m_input[m_index]=='['
 																||m_input[m_index]==']'
+																||m_input[m_index]=='~'
 																||m_input[m_index]=='+'
 																||m_input[m_index]=='-'
 																||m_input[m_index]=='*'
